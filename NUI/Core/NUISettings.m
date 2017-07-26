f9d4fb067b9a31b5fbe7d530924fda9fffc48204
xtinger <xtinger@bk.ru>
2017-07-26 12:18:45 +0300

    cleanup

--- NUI/Core/NUISettings.m
+++ NUI/Core/NUISettings.m
@@ -232,22 +232,20 @@ static NUISettings *instance = nil;
         NSString *fontName = [self get:propertyName withClass:className];
         
 #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 80200
-        if (&UIFontWeightBlack != NULL) {
-            if ([fontName isEqualToString:@"blackSystem"]) {
-                font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightBlack];
-            } else if ([fontName isEqualToString:@"heavySystem"]) {
-                font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightHeavy];
-            } else if ([fontName isEqualToString:@"lightSystem"]) {
-                font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightLight];
-            } else if ([fontName isEqualToString:@"mediumSystem"]) {
-                font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightMedium];
-            } else if ([fontName isEqualToString:@"semiboldSystem"]) {
-                font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightSemibold];
-            } else if ([fontName isEqualToString:@"thinSystem"]) {
-                font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightThin];
-            } else if ([fontName isEqualToString:@"ultraLightSystem"]) {
-                font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightUltraLight];
-            }
+        if ([fontName isEqualToString:@"blackSystem"]) {
+            font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightBlack];
+        } else if ([fontName isEqualToString:@"heavySystem"]) {
+            font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightHeavy];
+        } else if ([fontName isEqualToString:@"lightSystem"]) {
+            font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightLight];
+        } else if ([fontName isEqualToString:@"mediumSystem"]) {
+            font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightMedium];
+        } else if ([fontName isEqualToString:@"semiboldSystem"]) {
+            font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightSemibold];
+        } else if ([fontName isEqualToString:@"thinSystem"]) {
+            font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightThin];
+        } else if ([fontName isEqualToString:@"ultraLightSystem"]) {
+            font = [UIFont systemFontOfSize:fontSize weight:UIFontWeightUltraLight];
         }
 #endif
         if (!font) {
